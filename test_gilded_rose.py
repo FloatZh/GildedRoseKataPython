62d65c30d0a860bb47eac5804093c0ce60fef812
Kefan Zhou <zhou.kefa@northeastern.edu>
2025-02-05 23:57:40 -0800

    update unit tests

--- test_gilded_rose.py
+++ test_gilded_rose.py
@@ -5,11 +5,47 @@ from gilded_rose import Item, GildedRose
 
 
 class GildedRoseTest(unittest.TestCase):
-    def test_foo(self):
-        items = [Item("foo", 0, 0)]
+
+    # # example of test that checks for logical errors
+    # def test_sulfuras_should_not_decrease_quality(self):
+    #     items = [Item("Sulfuras", 5, 80)]
+    #     gilded_rose = GildedRose(items)
+    #     gilded_rose.update_quality()
+    #     sulfuras_item = items[0]
+    #     self.assertEqual(80, sulfuras_item.quality)
+    #     self.assertEqual(4, sulfuras_item.sell_in)
+    #     self.assertEqual("Sulfuras", sulfuras_item.name)
+
+    # # example of test that checks for syntax errors
+    # def test_gilded_rose_list_all_items(self):
+    #     items = [Item("Sulfuras", 5, 80)]
+    #     gilded_rose = GildedRose(items)
+    #     all_items = gilded_rose.get_item()
+    #     self.assertEqual(["Sulfuras"], all_items)
+
+    def test_conjured_items_degrade_twice_as_fast(self):
+        items = [Item(name="ConjuredItem", sell_in=2, quality=8)]
         gilded_rose = GildedRose(items)
         gilded_rose.update_quality()
-        self.assertEquals("fixme", items[0].name)
+        self.assertEqual(items[0].quality, 6)
+
+    def test_quality_never_negative(self):
+        items = [Item(name="ABC", sell_in=5, quality=0.5)]
+        gilded_rose = GildedRose(items)
+        gilded_rose.update_quality()
+        self.assertGreaterEqual(items[0].quality, 0)
+
+    def test_quality_never_exceed_50(self):
+        items = [Item(name="Aged Brie", sell_in=5, quality=49.5)]
+        gilded_rose = GildedRose(items)
+        gilded_rose.update_quality()
+        self.assertLess(items[0].quality, 50)
+
+    def test_calling_non_existent_method(self):
+        items = [Item(name="Aged Brie", sell_in=2, quality=10)]
+        gilded_rose = GildedRose(items)
+        gilded_rose.non_existent_method()
+
 
 
 if __name__ == '__main__':
